71b0158beeba06482091f21dd437a5138d51860b
Esteban Carreño <juanestebancarrenocamelo@gmail.com>
2022-10-20 17:02:00 -0500

    end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useRef, useState } from "react";
+import io from "socket.io-client";
 
 function App() {
+  const ref = useRef();
+  const [name, setName] = useState("");
+  const [isVisible, setIsVisible] = useState(true);
+  const [message, setMessage] = useState("");
+  const [allMessage, setAllMessage] = useState([]);
+
+  useEffect(() => {
+    ref.current = io("http://localhost:8080");
+
+    ref.current.on("message", (data) => {
+      console.log("WELCOME:", data.message);
+    });
+
+    ref.current.on("broadcast", (data) => {
+      console.log(data);
+      setAllMessage((prev) => [...prev, data]);
+    });
+
+    return () => {
+      ref.current.close();
+      ref.current.removeAllListeners();
+    };
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(message.length > 0){
+      ref.current.emit("message", { name, val: message });
+      setMessage("");
+    }
+
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {isVisible ? (
+        <>
+          <div className="opacity"></div>
+          <div className="main-container-modal-name-user">
+            <label htmlFor="name-user">Your Name</label>
+            <input
+              onChange={(e) => setName(e.target.value)}
+              placeholder="type your name"
+              type="text"
+              id="name-user"
+              className="input-modal-name-user"
+            />
+            <button
+              onClick={() => {
+                if (name.length >= 3) {
+                  setIsVisible(false);
+                }
+              }}
+              type="button"
+              className="btn-modal-name-user"
+            >
+              Guardar
+            </button>
+          </div>
+        </>
+      ) : null}
+      <div className="main-container-live-chat">
+        <div className="container-live-chat-view">
+          {allMessage.map((item) => (
+            <p key={item.val}>
+              {item.name}: {item.val}
+            </p>
+          ))}
+        </div>
+        <form
+          className="container-live-chat-input"
+          onSubmit={(e) => handleSubmit(e)}
         >
-          Learn React
-        </a>
-      </header>
+          <input
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            className="live-chat-input"
+            type="text"
+            placeholder="message"
+          />
+          <button className="live-chat-btn" type="submit">
+            Send
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
